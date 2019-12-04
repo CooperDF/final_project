03ccb07dd58600901f5821e2d52a5aacef63691c
Cooper French <cooper_df@outlook.com>
2019-12-04 07:48:09 -0700

    first commit

---
+++ js/player.js
@@ -0,0 +1,122 @@
+var song;
+var x = 1;
+
+function preload() {
+    soundFormats('mp3', 'ogg');
+    song1();
+    song2();
+    song3();
+    song4();
+    song5();
+    song6();
+}
+
+function setup() {
+    var canvas = createCanvas(0, 0);
+    canvas.parent('player');
+    vol();
+    buttons();
+    song.setVolume(0);
+    
+}
+
+function draw() {
+
+//    song.noLoop();
+}
+
+function song1() {
+    song = loadSound('/sounds/songs/C418 - stranger_think.mp3');
+    song.setVolume(0);
+    document.getElementById('sidenav').style.width = "0"
+
+}
+
+function song2() {
+    song = loadSound('/sounds/songs/Tuxedo - Do It.mp3');
+    song.setVolume(0);
+    document.getElementById('sidenav').style.width = "0";
+}
+
+function song3() {
+    song = loadSound('/sounds/songs/Plastic_Love_-_Caitlin_Myers.mp3');
+    song.setVolume(0);
+    document.getElementById('sidenav').style.width = "0";
+    
+}
+
+function song4() {
+    song = loadSound('/sounds/songs/Sunrise (Final).mp3');
+    song.setVolume(0);
+    document.getElementById('sidenav').style.width = "0";
+}
+
+function song5() {
+    song = loadSound('/sounds/songs/Eli Way - Wave.mp3');
+    song.setVolume(0);
+    document.getElementById('sidenav').style.width = "0";
+}
+
+function song6() {
+    song = loadSound('/sounds/songs/Jack Stauber - Buttercup.mp3');
+    song.setVolume(0);
+    document.getElementById('sidenav').style.width = "0";
+}
+
+function buttons() {
+    document.getElementById('play').addEventListener('click', function(){
+        x = 1;
+        song.playMode('untilDone');
+        song.rate(x);
+        song.play();
+        document.getElementById('ff').addEventListener('click', function() {
+            x = 3;
+            song.rate(x);
+        });
+        document.getElementById('rew').addEventListener('click', function(rewind) {
+//            x = -2;
+//            var rev = song.duration() - song.currentTime();
+//            console.log(rev);
+////            somg.play();
+            song.rate(-song.currentTime()/2);
+        });
+    });
+
+    document.getElementById('stop').addEventListener('click', function(){
+        x = 1;
+        song.rate(x);
+        song.pause();
+    });
+    document.getElementById('pause').addEventListener('click', function(){
+        x = 1;
+        song.rate(x);
+       song.pause(); 
+    });
+    
+}
+
+
+//function rec() {
+//    var rbutt = document.getElementById('record');
+//    mic = new p5.AudioIn();
+//    
+//    rbutt.addEventListener('click', function(){
+//        mic.start();
+//        
+//        recorder = new p5.SoundRecorder();
+//        
+//        recorder.setInput(mic);
+//        
+//        soundFile = new p5.SoundFile();
+//        
+//    }); 
+//}
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "350px";
+    song.stop();
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
---
+++ js/sounds.js
@@ -0,0 +1,54 @@
+function play(url) {
+    new Audio(url).play();
+}
+
+function stop(url1) {
+    new Audio(url1).play();
+}
+
+function rew(url2) {
+    new Audio(url2).play();
+}
+
+function ff(url3) {
+    new Audio(url3).play();
+}
+
+function pause(url4) {
+    new Audio(url4).play();
+}
+
+function rec(url5) {
+    new Audio(url5).play();
+}
+
+function noisyNav(url6) {
+    new Audio(url6).play();
+}
+function noisyNav2(url6) {
+    new Audio(url6).play();
+}
+
+function select1(url7) {
+    new Audio(url7).play();
+}
+
+function select2(url8) {
+    new Audio(url8).play();
+}
+
+function select3(url9) {
+    new Audio(url9).play();
+}
+
+function select4(url10) {
+    new Audio(url10).play();
+}
+
+function select5(url11) {
+    new Audio(url11).play();
+}
+
+function select6(url12) {
+    new Audio(url12).play();
+}
