760d1aa2886d1230de3994999ada16e2a9456efd
Cooper French <cooper_df@outlook.com>
2020-01-05 11:03:49 -0700

    seventh commit

--- js/player.js
+++ js/player.js
@@ -42,39 +42,39 @@ function draw() {
 }
 
 function song1() {
-    song = loadSound('../sounds/songs/C418 - stranger_think.mp3');
+    song = loadSound('sounds/songs/C418 - stranger_think.mp3');
     song.setVolume(0);
     document.getElementById('sidenav').style.width = "0"
 
 }
 
 function song2() {
-    song = loadSound('../sounds/songs/Tuxedo - Do It.mp3');
+    song = loadSound('sounds/songs/Tuxedo - Do It.mp3');
     song.setVolume(0);
     document.getElementById('sidenav').style.width = "0";
 }
 
 function song3() {
-    song = loadSound('../sounds/songs/Plastic_Love_-_Caitlin_Myers.mp3');
+    song = loadSound('sounds/songs/Plastic_Love_-_Caitlin_Myers.mp3');
     song.setVolume(0);
     document.getElementById('sidenav').style.width = "0";
     
 }
 
 function song4() {
-    song = loadSound('../sounds/songs/Sunrise (Final).mp3');
+    song = loadSound('sounds/songs/Sunrise (Final).mp3');
     song.setVolume(0);
     document.getElementById('sidenav').style.width = "0";
 }
 
 function song5() {
-    song = loadSound('../sounds/songs/Eli Way - Wave.mp3');
+    song = loadSound('sounds/songs/Eli Way - Wave.mp3');
     song.setVolume(0);
     document.getElementById('sidenav').style.width = "0";
 }
 
 function song6() {
-    song = loadSound('../sounds/songs/Jack Stauber - Buttercup.mp3');
+    song = loadSound('sounds/songs/Jack Stauber - Buttercup.mp3');
     song.setVolume(0);
     document.getElementById('sidenav').style.width = "0";
 }
