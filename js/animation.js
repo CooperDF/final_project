77ad1ea0c185090a6efe81034ccadd4837dd06a5
Cooper French <cooper_df@outlook.com>
2020-01-10 00:39:47 -0700

    twelfth commit

--- js/animation.js
+++ js/animation.js
@@ -1,117 +1,118 @@
-
-
 gsap.registerPlugin(Draggable);
 
 gsap.set("#volume-nob", {
     transformOrigin: "50% 50%"
 });
 
-gsap.set("#cassette", {transformOrigin: "50% 0%",translateY: 800})
+gsap.set("#cassette", {
+    transformOrigin: "50% 0%",
+    translateY: 800
+})
 
 
 function vol() {
-var volume = 0;
+    var volu = 0;
 
-var drag = Draggable.create("#volume-nob", {
-    type: "rotation",
-    bounds:{
-        minRotation:0,
-        maxRotation:300
-    },
-    onDrag: function() {
-        var progress = normalize(this.rotation, 0, 300);
-        
-        volume = progress;
-//        console.log("progress =", volume);
-        song.setVolume(volume);
-            if (volume > 0.1) {
+    var drag = Draggable.create("#volume-nob", {
+        type: "rotation",
+        bounds: {
+            minRotation: 0,
+            maxRotation: 300
+        },
+        onDrag: function () {
+            var progress = normalize(this.rotation, 0, 300);
+
+            volu = progress;
+                    console.log("progress =", volu);
+            song.setVolume(volu);
+            if (volu > 0.1) {
                 TweenLite.to("#_Image8", 0.5, {
-   fill: "rgb(255,0,0)"
-});
+                    fill: "rgb(255,0,0)"
+                });
             }
-    }
-});
+        }
+    });
 
-function normalize(value, min, max) {
-    return (value - min) / (max - min);
-}
-    
+    function normalize(value, min, max) {
+        return (value - min) / (max - min);
+    }
+
 }
 
 
 var cog1 = TweenMax.to("#cog1", {
-    rotation:-360,
+    rotation: -360,
     transformOrigin: "50% 50%",
-    duration:2,
+    duration: 2,
     ease: Linear.easeNone,
-    repeat:-1,
+    repeat: -1,
     paused: true,
     yoyo: false
 });
 var cog2 = TweenMax.to("#cog2", {
-    rotation:-360,
+    rotation: -360,
     transformOrigin: "50% 50%",
-    duration:2,
+    duration: 2,
     ease: Linear.easeNone,
-    repeat:-1,
+    repeat: -1,
     paused: true,
     yoyo: false
 });
 var cog3 = TweenMax.to("#cog3", {
-    rotation:-360,
+    rotation: -360,
     transformOrigin: "50% 50%",
-    duration:2,
+    duration: 2,
     ease: Linear.easeNone,
-    repeat:-1,
+    repeat: -1,
     paused: true,
     yoyo: false
-}); 
+});
 var cog4 = TweenMax.to("#cog4", {
-    rotation:-360,
+    rotation: -360,
     transformOrigin: "50% 50%",
-    duration:2,
+    duration: 2,
     ease: Linear.easeNone,
-    repeat:-1,
+    repeat: -1,
     paused: true,
     yoyo: false
 });
 
 var cods1 = TweenMax.to("#cog1", {
-    rotation:360,
+    rotation: 360,
     transformOrigin: "50% 50%",
-    duration:2,
+    duration: 2,
     ease: Linear.easeNone,
-    repeat:-1,
+    repeat: -1,
     paused: true
 });
 var cods2 = TweenMax.to("#cog2", {
-    rotation:360,
+    rotation: 360,
     transformOrigin: "50% 50%",
-    duration:2,
+    duration: 2,
     ease: Linear.easeNone,
-    repeat:-1,
+    repeat: -1,
     paused: true
 });
 var cods3 = TweenMax.to("#cog3", {
-    rotation:360,
+    rotation: 360,
     transformOrigin: "50% 50%",
-    duration:2,
+    duration: 2,
     ease: Linear.easeNone,
-    repeat:-1,
+    repeat: -1,
     paused: true
-}); 
+});
 var cods4 = TweenMax.to("#cog4", {
-    rotation:360,
+    rotation: 360,
     transformOrigin: "50% 50%",
-    duration:2,
+    duration: 2,
     ease: Linear.easeNone,
-    repeat:-1,
+    repeat: -1,
     paused: true
 });
 
-document.getElementById('play').addEventListener('click', function(playing){
+document.getElementById('play').addEventListener('click', function (playing) {
     playing.preventDefault();
-    if(playing){
+    if (playing) {
         cog1.play();
         cog2.play();
         cog3.play();
@@ -127,133 +128,154 @@ document.getElementById('play').addEventListener('click', function(playing){
     }
 });
 
-document.getElementById('ff').addEventListener('click', function(faster) {
-    faster.preventDefault();    
-       if(faster) {
-           cog1.play();
+document.getElementById('ff').addEventListener('click', function (faster) {
+    faster.preventDefault();
+    if (faster) {
+        cog1.play();
         cog2.play();
         cog3.play();
         cog4.play();
-           cog1.duration(0.5);
-           cog2.duration(0.5);
-           cog3.duration(0.5);
-           cog4.duration(0.5);
-           cods1.paused(true);
+        cog1.duration(0.5);
+        cog2.duration(0.5);
+        cog3.duration(0.5);
+        cog4.duration(0.5);
+        cods1.paused(true);
         cods2.paused(true);
         cods3.paused(true);
         cods4.paused(true);
-       }
+    }
 });
 
-document.getElementById('rew').addEventListener('click', function(rewind) {
+document.getElementById('rew').addEventListener('click', function (rewind) {
     rewind.preventDefault();
-        cods1.play();
-        cods2.play();
-        cods3.play();
-        cods4.play();
-        cods1.duration(0.5);
-        cods2.duration(0.5);
-        cods3.duration(0.5);
-        cods4.duration(0.5);
+    cods1.play();
+    cods2.play();
+    cods3.play();
+    cods4.play();
+    cods1.duration(0.5);
+    cods2.duration(0.5);
+    cods3.duration(0.5);
+    cods4.duration(0.5);
 
 })
 
-document.getElementById('stop').addEventListener('click', function(stopped) {
-    stopped.preventDefault();    
-       if(stopped) {
-           cog1.paused(true);
-           cog2.paused(true);
-           cog3.paused(true);
-           cog4.paused(true);
-           cods1.paused(true);
+document.getElementById('stop').addEventListener('click', function (stopped) {
+    stopped.preventDefault();
+    if (stopped) {
+        cog1.paused(true);
+        cog2.paused(true);
+        cog3.paused(true);
+        cog4.paused(true);
+        cods1.paused(true);
         cods2.paused(true);
         cods3.paused(true);
         cods4.paused(true);
-       }
-    document.getElementById('stop').addEventListener('dblclick', function(){
-        gsap.set("#cassette", {transformOrigin: "50% 0%"})
-        gsap.to("#cassette", 1,{
-            translateY: 800});
+    }
+    document.getElementById('stop').addEventListener('dblclick', function () {
+        gsap.set("#cassette", {
+            transformOrigin: "50% 0%"
+        })
+        gsap.to("#cassette", 1, {
+            translateY: 800
+        });
     });
 });
 
-document.getElementById('pause').addEventListener('click', function(paused) {
-    paused.preventDefault();    
-       if(paused) {
-           cog1.paused(true);
-           cog2.paused(true);
-           cog3.paused(true);
-           cog4.paused(true);
-           cods1.paused(true);
+document.getElementById('pause').addEventListener('click', function (paused) {
+    paused.preventDefault();
+    if (paused) {
+        cog1.paused(true);
+        cog2.paused(true);
+        cog3.paused(true);
+        cog4.paused(true);
+        cods1.paused(true);
         cods2.paused(true);
         cods3.paused(true);
         cods4.paused(true);
-     
-       }
+
+    }
 });
 
 
-document.getElementById("logo").addEventListener('click', function(stopped) {
+document.getElementById("logo").addEventListener('click', function (stopped) {
     stopped.preventDefault();
-    if(stopped) {
-           cog1.paused(true);
-           cog2.paused(true);
-           cog3.paused(true);
-           cog4.paused(true);
-        gsap.set("#cassette", {transformOrigin: "50% 0%"})
-        gsap.to("#cassette", 1,{
-            translateY: 800});
-       }
+    if (stopped) {
+        cog1.paused(true);
+        cog2.paused(true);
+        cog3.paused(true);
+        cog4.paused(true);
+        gsap.set("#cassette", {
+            transformOrigin: "50% 0%"
+        })
+        gsap.to("#cassette", 1, {
+            translateY: 800
+        });
+    }
 });
 
-document.getElementById('tapes1').addEventListener('click', function(putback) {
-    if(putback) {
-    gsap.set("#cassette", {transformOrigin: "50% 0%"})
-        gsap.to("#cassette", 1,{
-            translateY: 0});
+document.getElementById('tapes1').addEventListener('click', function (putback) {
+    if (putback) {
+        gsap.set("#cassette", {
+            transformOrigin: "50% 0%"
+        })
+        gsap.to("#cassette", 1, {
+            translateY: 0
+        });
         new Audio('/sounds/click1.ogg').play();
     }
 })
 
-document.getElementById('tapes2').addEventListener('click', function(putback1) {
-    if(putback1) {
-    gsap.set("#cassette", {transformOrigin: "50% 0%"})
-        gsap.to("#cassette", 1,{
-            translateY: 0});
+document.getElementById('tapes2').addEventListener('click', function (putback1) {
+    if (putback1) {
+        gsap.set("#cassette", {
+            transformOrigin: "50% 0%"
+        })
+        gsap.to("#cassette", 1, {
+            translateY: 0
+        });
     }
 })
 
-document.getElementById('tapes3').addEventListener('click', function(putback2) {
-    if(putback2) {
-    gsap.set("#cassette", {transformOrigin: "50% 0%"})
-        gsap.to("#cassette", 1,{
-            translateY: 0});
+document.getElementById('tapes3').addEventListener('click', function (putback2) {
+    if (putback2) {
+        gsap.set("#cassette", {
+            transformOrigin: "50% 0%"
+        })
+        gsap.to("#cassette", 1, {
+            translateY: 0
+        });
     }
 })
 
-document.getElementById('tapes4').addEventListener('click', function(putback3) {
-    if(putback3) {
-    gsap.set("#cassette", {transformOrigin: "50% 0%"})
-        gsap.to("#cassette", 1,{
-            translateY: 0});
+document.getElementById('tapes4').addEventListener('click', function (putback3) {
+    if (putback3) {
+        gsap.set("#cassette", {
+            transformOrigin: "50% 0%"
+        })
+        gsap.to("#cassette", 1, {
+            translateY: 0
+        });
     }
 })
 
-document.getElementById('tapes5').addEventListener('click', function(putback4) {
-    if(putback4) {
-    gsap.set("#cassette", {transformOrigin: "50% 0%"})
-        gsap.to("#cassette", 1,{
-            translateY: 0});
+document.getElementById('tapes5').addEventListener('click', function (putback4) {
+    if (putback4) {
+        gsap.set("#cassette", {
+            transformOrigin: "50% 0%"
+        })
+        gsap.to("#cassette", 1, {
+            translateY: 0
+        });
     }
 })
 
-document.getElementById('tapes6').addEventListener('click', function(putback5) {
-    if(putback5) {
-    gsap.set("#cassette", {transformOrigin: "50% 0%"})
-        gsap.to("#cassette", 1,{
-            translateY: 0});
+document.getElementById('tapes6').addEventListener('click', function (putback5) {
+    if (putback5) {
+        gsap.set("#cassette", {
+            transformOrigin: "50% 0%"
+        })
+        gsap.to("#cassette", 1, {
+            translateY: 0
+        });
     }
 })
-
-        
-
--- js/player.js
+++ js/player.js
@@ -6,6 +6,8 @@ var x = 1;
 //var recorder;
 //var soundFile;
 
+var vols = document.querySelector("audio").volume;
+
 function preload() {
     soundFormats('mp3', 'ogg');
     song1();
@@ -18,114 +20,103 @@ function preload() {
 
 function setup() {
     var canvas = createCanvas(0, 0);
-//    mic = new p5.AudioIn();
-//    micOn = false;
-    
+    //    mic = new p5.AudioIn();
+    //    micOn = false;
+
     canvas.parent('player');
-    
-//    mic.start();
-    
-//    recorder = new p5.SoundRecorder();
-//    
-//    recorder.setInput(mic);
-//    soundFile = new p5.SoundFile();
+
+    //    mic.start();
+
+    //    recorder = new p5.SoundRecorder();
+    //    
+    //    recorder.setInput(mic);
+    //    soundFile = new p5.SoundFile();
     vol();
     buttons();
-    
-//    song.setVolume(0);
     
 }
 
 function draw() {
-
-//    song.noLoop();
+    //    song.noLoop();
+//    song.setVolume(volu);
 }
 
 function song1() {
     song = loadSound('sounds/songs/C418-stranger-think.mp3');
-    song.setVolume(0);
-    document.getElementById('sidenav').style.width = "0"
+    document.getElementById('sidenav').style.width = "0";
 
 }
 
 function song2() {
     song = loadSound('sounds/songs/Tuxedo-Do-It.mp3');
-    song.setVolume(0);
     document.getElementById('sidenav').style.width = "0";
 }
 
 function song3() {
     song = loadSound('sounds/songs/PlasticLove-Caitlin-Myers.mp3');
-    song.setVolume(0);
     document.getElementById('sidenav').style.width = "0";
-    
 }
 
 function song4() {
     song = loadSound('sounds/songs/Sunrise-Final.mp3');
-    song.setVolume(0);
     document.getElementById('sidenav').style.width = "0";
 }
 
 function song5() {
     song = loadSound('sounds/songs/Eli-Way-Wave.mp3');
-    song.setVolume(0);
     document.getElementById('sidenav').style.width = "0";
 }
 
 function song6() {
     song = loadSound('sounds/songs/Jack-Stauber-Buttercup.mp3');
-    song.setVolume(0);
     document.getElementById('sidenav').style.width = "0";
 }
 
 function buttons() {
-    document.getElementById('play').addEventListener('click', function(){
+    document.getElementById('play').addEventListener('click', function () {
         x = 1;
+        song.setVolume(vols);
         song.playMode('untilDone');
         song.rate(x);
         song.play();
-        document.getElementById('ff').addEventListener('click', function() {
+    });
+            document.getElementById('ff').addEventListener('click', function () {
             x = 4;
             song.rate(x);
         });
-        document.getElementById('rew').addEventListener('click', function(rewind) {
-//            x = -2;
-//            var rev = song.duration() - song.currentTime();
-//            console.log(rev);
-////            somg.play();
-            song.rate(-song.currentTime()/2);
+        document.getElementById('rew').addEventListener('click', function (rewind) {
+            //            x = -2;
+            //            var rev = song.duration() - song.currentTime();
+            //            console.log(rev);
+            ////            somg.play();
+            song.rate(-song.currentTime() / 2);
         });
-    });
 
-    document.getElementById('stop').addEventListener('click', function(){
+    document.getElementById('stop').addEventListener('click', function () {
         x = 1;
         song.rate(x);
         song.pause();
         recorder.stop();
     });
-    document.getElementById('pause').addEventListener('click', function(){
+    document.getElementById('pause').addEventListener('click', function () {
         x = 1;
         song.rate(x);
-       song.pause(); 
+        song.pause();
     });
-    document.getElementById('record').addEventListener('click', function() {
+    document.getElementById('record').addEventListener('click', function () {
         alert("Maybe one day there will be the technology to record sound!");
-//        micOn = !micOn;
-//        
-//        if (micOn) {
-//            mic.start;
-//            recorder.record(soundFile);
-//        }else{
-//            mic.stop();
-//            recorder.stop();
-//        }
-});
-    
+        //        micOn = !micOn;
+        //        
+        //        if (micOn) {
+        //            mic.start;
+        //            recorder.record(soundFile);
+        //        }else{
+        //            mic.stop();
+        //            recorder.stop();
+        //        }
+    });
+
 }
-
-
-
 
 function openNav() {
     document.getElementById("sidenav").style.width = "350px";
